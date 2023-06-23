89635844d652b135ff5b460447fa3ee1ccbe4881
Alberto Chiusole <chiusole@seqera.io>
2023-06-23 17:27:07 +0200

    Add gclib ("common" header files); make gffread depend on gclib

--- var/spack/repos/builtin/packages/gffread/package.py
+++ var/spack/repos/builtin/packages/gffread/package.py
@@ -19,15 +19,25 @@ class Gffread(Package):
     different gene annotations."""
 
     homepage = "http://ccb.jhu.edu/software/stringtie/gff.shtml"
-    url = "https://github.com/gpertea/gffread/archive/refs/tags/v0.12.7.zip"
+    url = "https://github.com/gpertea/gffread/archive/refs/tags/v0.12.7.tar.gz"
 
-    version("0.12.7", sha256="93921c27f7560d728122855ab1f0bb034ba569fb1a5c8a89043122cdedef34c2")
+    version("0.12.7", sha256="6f3417090b3beb74f435bf5d462c87475b3c704a8a0d71821f6c1321c47dbbbe")
 
+    # Not sure whether gclib and gffread versions should match to work. Tried matching 0.12.1 but
+    # gffread breaks. It seems gclib is a repo storing shared header files, and a new version is
+    # cut on all dependencies when a new version of the library is made. Force match for now.
+    depends_on("gclib@0.12.7", when="@0.12.7", type=("build", "link"))
+
+    phases = ["build", "install"]
     executables = ["gffread"]
 
+    def setup_build_environment(self, env):
+        env.set("GCLDIR", self.spec['gclib'].prefix.include)
+
+    def build(self, spec, prefix):
+        make("gffread")
+
     def install(self, spec, prefix):
-        make("../gclib")
-        make("release")
         mkdirp(prefix.bin)
         install("gffread", prefix.bin)
 
