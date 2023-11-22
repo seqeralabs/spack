5c2cf79a37aa1281583af60bf8394c34736f0060
Dr Marco Claudio De La Pierre <marco.delapierre@seqera.io>
2023-11-22 10:55:40 +0800

    bioc4x: minor update to version list
    Signed-off-by: Dr Marco Claudio De La Pierre <marco.delapierre@seqera.io>

--- var/spack/repos/builtin/packages/r-rsubread/package.py
+++ var/spack/repos/builtin/packages/r-rsubread/package.py
@@ -11,8 +11,8 @@ class RRsubread(RPackage):
 
     bioc = "Rsubread"
 
-    version("2.14.2", commit="863bd98c6523b888da59335a6acb516d2676d412")
     version("2.16.0", commit="62b92c9ed3fc2be89ed9f29e3db1809d1e115dbc")
+    version("2.14.2", commit="863bd98c6523b888da59335a6acb516d2676d412")
 
     depends_on('r', type=('build', 'run'))
     depends_on("r-matrix", type=('build', 'run'))
